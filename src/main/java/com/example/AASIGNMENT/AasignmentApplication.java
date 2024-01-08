cc566595672c3e4cf5f412cd7d4d221bb1c56d0a
dhawal543 <137651549+dhawal543@users.noreply.github.com>
2024-01-08 20:37:27 +0530

    Add files via upload

---
+++ src/main/java/com/example/AASIGNMENT/AasignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.AASIGNMENT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AasignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AasignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AASIGNMENT/AasignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AASIGNMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AasignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
